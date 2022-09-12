0da54eae364eff14097a2ddab598c2ddae2de107
Ark-of-God <michaelemmanuel258@gmail.com>
2022-09-12 13:19:57 -0700

    put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main - Entry point
  *
- * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle
- * Retun: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts('\"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
