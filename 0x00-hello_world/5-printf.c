d9e1dd15e5451a6fb0e7ae824836546b78d325ee
Ark-of-God <michaelemmanuel258@gmail.com>
2022-09-12 14:26:50 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: "proper grammer"
+ * Description: "with proper grammar, but the outcome is a piece of art"
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 { 
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art");
 	return (0);
 }
