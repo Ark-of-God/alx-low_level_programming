12edb31a24921d88c683262d648b5744ae7ecda0
Ark-of-God <michaelemmanuel258@gmail.com>
2022-09-13 18:56:27 -0700

    check lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _islower - tests whether a character is a 
- * lowercase letter from the English alphabet.
- * @c: is the int that will use for the argument of the function
- * Return: 0
+ *_islower - tests whether a character is a 
+ *lowercase letter from the English alphabet.
+ *@c: is the int that will use for the argument of the function
+ *Return: 0
  */
 int _islower(int c)
 {
