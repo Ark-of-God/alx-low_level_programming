8d5ac2635a4eb4167c50e60a6b914bfda9129e5e
Ark-of-God <michaelemmanuel258@gmail.com>
2022-09-13 20:12:52 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,12 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last digit *= -1;
+
+	_putchar(last_digit + '0');
+
 	return (n % 10);
 }
