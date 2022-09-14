8d040e3f3f66cff966e18a57e2c29dea93b9908e
Ark-of-God <michaelemmanuel258@gmail.com>
2022-09-13 21:31:49 -0700

    print every minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,18 +5,26 @@
  */
 void jack_bauer(void)
 {
-	int h, m;
+	int hours = 0;
+       int minutes = 0;
+       int hours_remainder;
+       int mins_remainder;
 
-	for (h = 0; h < 24;	
+	while (hours <= 23)
 	{
-		for (m = 0; m < 60; m++)
-		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
-			_putchar(':');
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
-			_putchar('\n');
-		}
+	while (minutes <= 59)
+	{
+	mins_remainder = minutes % 10;
+	hours_remainder = hours % 10;
+	_putchar(hours / 10 + '0');
+	_putchar((hours_remainder + '0');
+	_putchar(':');
+	_putchar(minutes / 10 + '0');
+	_putchar((min_remainder + '0');
+	minutes++;
+	_putchar('\n');
 	}
+	hours++;
+	minutesb = 0;
+}
 }
