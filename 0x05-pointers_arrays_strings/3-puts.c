885da007b2496d32c760a8e0b4abe40e0af79574
Ark-of-God <michaelemmanuel258@gmail.com>
2022-09-20 01:12:36 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include <studio.h>
+#include "main.h"
+
+/**
+ *_puts - prints a string
+ *@str: string to print
+ *
+ * Description: prints a string
+ * 0n success: returns no error
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+}
