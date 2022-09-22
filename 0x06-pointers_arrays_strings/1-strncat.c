50d60ba34f877d686390ed992033aaa0301a1470
Ark-of-God <michaelemmanuel258@gmail.com>
2022-09-22 16:14:47 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strncat - concatenate two strings but add inputted number of bytes
  * @dest: string to be apended upon
@@ -16,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 		dest_len++;
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[destr_len++] = src[index];
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
