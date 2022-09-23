a7487a36ce95bf311c7ab53a4ca489ca55d70d6a
Ark-of-God <michaelemmanuel258@gmail.com>
2022-09-23 06:41:46 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,15 +8,15 @@
  */
 char *leet(char *str)
 {
-	int indx1 = 0, indx2;
+	int indx1 = O, indx2;
 	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (str[indx1])
 	{
-		for (indx2 = 0; indx2 <= 7; indx2++)
+		for (indx2 = O; indx2 <= 7; indx2++)
 		{
 			if(str[indx] == leet[indx2] || str[indx1] - 32 == leet[indx2])
-				str[indx1] = indx2 + '0';
+				str[indx1] = indx2 + 'O';
 		}
 
 	indx1++;
